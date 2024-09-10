40132eed3566c5c71ace3175dbc68ebc562182d7
hhabtia <hhabtia@gmail.com>
2024-09-10 14:14:02 +0700

    Add files via upload

---
+++ MataPelajaran.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuanketiga;
+
+/**
+ *
+ * @author Habtia Humaira
+ */
+public abstract class MataPelajaran {
+
+    abstract void mataPelajaran();
+
+    public void tujuan() {
+        System.out.println("Tujuan adanya mata pelajaran adalah untuk mengembangkan potensi siswa secara maksimal.");
+    }
+}
---
+++ Matematika.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuanketiga;
+
+/**
+ *
+ * @author Habtia Humaira
+ */
+public class Matematika extends MataPelajaran implements Mulai, Selesai {
+
+    String guru;
+    int jumlahJam;
+
+    public Matematika() {
+        System.out.println("Aku adalah matematika");
+        super.tujuan();
+    }
+
+    public void siswa() {
+        System.out.println("Siswa tidak terlalu suka matematika");
+    }
+
+    public void siswa(String eksklusif) {
+        System.out.println("Ada beberapa siswa yang sangat suka dengan matematika");
+    }
+
+    public String siswa(int jumlahJam) {
+        return "Jumlah jam mata pelajaran ini adalah " + jumlahJam + " jam";
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Kelas baru saja dimulai");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Kelas sudah selesai");
+    }
+
+    @Override
+    void mataPelajaran() {
+        System.out.println("Ini adalah mata pelajaran");
+    }
+}
