fb8ca717c79da3bf697e586e445ab00fbda9b5cc
hhabtia <hhabtia@gmail.com>
2024-09-10 11:15:41 +0700

    Add files via upload

--- MataPelajaran.java
+++ MataPelajaran.java
@@ -2,52 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pertemuanketigaa;
+package pertemuanketiga;
 
 /**
  *
  * @author Habtia Humaira
  */
-public class MataPelajaran extends Kurikulum {
-    private String namaMapel;
-    private int jumlahJam;
-
-    @Override
-    void menuntutIlmu() {
-        System.out.println("Menuntut ilmu itu penting");
-    }
-
-    @Override
-    void belajar() {
-        System.out.println("Belajarlah yang rajin");
-    }
-
-    /**
-     * @return the namaMapel
-     */
-    public String getNamaMapel() {
-        return namaMapel;
-    }
-
-    /**
-     * @param namaMapel the namaMapel to set
-     */
-    public void setNamaMapel(String namaMapel) {
-        this.namaMapel = namaMapel;
-    }
-
-    /**
-     * @return the jumlahJam
-     */
-    public int getJumlahJam() {
-        return jumlahJam;
-    }
-
-    /**
-     * @param jumlahJam the jumlahJam to set
-     */
-    public void setJumlahJam(int jumlahJam) {
-        this.jumlahJam = jumlahJam;
-    }
+public abstract class MataPelajaran {
+    String nama;
+    abstract void MataPelajaran(); 
     
+    public void tujuan() {
+        System.out.println("Tujuan adanya mata pelajaran adalah untuk mengembangkan potensi siswa secara maksimal.");
+    } 
 }
--- Matematika.java
+++ Matematika.java
@@ -2,75 +2,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pertemuanketigaa;
+package pertemuanketiga;
 
 /**
  *
  * @author Habtia Humaira
  */
-public class Matematika extends MataPelajaran implements Minat, Wajib {
-    
-    private String topik;
-    private String guru;
-    
-    public Matematika(){
+public class Matematika extends MataPelajaran implements Mulai, Selesai {
+
+    String guru;
+    int jumlahJam;
+
+    public Matematika() {
+        System.out.println("Aku adalah matematika");
         super.tujuan();
-        System.out.println("Tujuan belajar matematika adalah mendorong kemampuan problem solving.");
-        this.setTopik("Geometri");
-        this.setGuru("PakBudi");
-        super.setNamaMapel("MatematikaWajib");
-        super.setJumlahJam(3);
-
+        MataPelajaran();
     }
 
-    /**
-     * @return the topik
-     */
-    public String getTopik() {
-        return topik;
+    public void siswa() {
+        System.out.println("Siswa tidak terlalu suka matematika");
     }
 
-    /**
-     * @param topik the topik to set
-     */
-    public void setTopik(String topik) {
-        this.topik = topik;
+    public void siswa(String eksklusif) {
+        System.out.println("Ada beberapa siswa yang sangat suka dengan matematika");
     }
 
-    /**
-     * @return the guru
-     */
-    public String getGuru() {
-        return guru;
-    }
-
-    /**
-     * @param guru the guru to set
-     */
-    public void setGuru(String guru) {
-        this.guru = guru;
+    public String siswa(int jumlahJam) {
+        return "Jumlah jam mata pelajaran ini adalah " + jumlahJam + " jam";
     }
 
     @Override
-    public void statistika() {
-        System.out.println("Statistika mencakup analisis dan penyajian data");
+    void MataPelajaran() {
+        System.out.println("Ini adalah mata pelajaran");
     }
 
     @Override
-    public void teoriPeluang() {
-        System.out.println("Teori Peluang mempelajari tentang kemungkinan terjadinya suatu peristiwa.");
+    public void start() {
+        System.out.println("Kelas baru saja dimulai");
     }
 
     @Override
-    public void aljabar() {
-        System.out.println("Aljabar adalah cabang matematika yang sangat luas dan mendalam.");
+    public void stop() {
+        System.out.println("Kelas sudah selesai");
     }
-
-    @Override
-    public void trigonometri() {
-        System.out.println(" Trigonometri adalah cabang ilmu matematika yang mempelajari hubungan antara sisi dan sudut pada segitiga");
-    }
-    }
-
-
-
+}
